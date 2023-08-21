402f7b1529998a440a4c20b9bb1d66f80d7104b8
JoseOLDG <joseoldg08@gmail.com>
2023-08-21 17:51:07 -0400

    xd

--- Prizus/core/views.py
+++ Prizus/core/views.py
@@ -19,7 +19,6 @@ def registro(request):
             formulario.save()
             user = authenticate(username=formulario.cleaned_data["username"], password=formulario.cleaned_data["password1"])
             login(request,user)
-            
             return redirect(to="index")
         data["form"] = formulario
     
