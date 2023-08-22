5860ed3115052413721e5d0930d6e604885d2152
sebfigueroap <82173094+sebfigueroap@users.noreply.github.com>
2023-08-21 22:54:22 -0400

    avance

--- Prizus/core/urls.py
+++ Prizus/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, login, registro
 
 urlpatterns = [
     path('', index, name="index"),
+    path('login/', login, name="login"),
+    path('registro/', registro, name="registro"),
 ]
--- Prizus/core/views.py
+++ Prizus/core/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def login(request):
+    return render(request, 'core/login.html')
+
+def registro(request):
+    return render(request, 'core/registro.html')
