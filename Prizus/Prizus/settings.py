a3c187fc0b2e508a393dc3351a550550e33402b0
sebfigueroap <82173094+sebfigueroap@users.noreply.github.com>
2023-08-23 20:54:50 -0400

    avance

--- Prizus/Prizus/settings.py
+++ Prizus/Prizus/settings.py
@@ -124,6 +124,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'resetprizus@gmail.com'
+EMAIL_HOST_PASSWORD = 'nkqzgcoubezwtpqd'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
