0ea4ed75e0171e7c37381d7e838dd57c23bb9c97
Ismael Cerda <gonzalo.cerda.xd@gmail.com>
2023-09-06 17:58:16 -0300

    agregado comentarios

--- Prizus/core/urls.py
+++ Prizus/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, login, registro, menu
+from .views import index, login, registro, menu, producto
 
 urlpatterns = [
     path('', index, name="index"),
     path('registro/', registro, name="registro"),
     path('menu/', menu, name="menu"),
+    path('producto/', producto, name="producto"),
 ]
--- Prizus/core/views.py
+++ Prizus/core/views.py
@@ -2,6 +2,7 @@ from pyexpat.errors import messages
 from django.shortcuts import redirect, render
 from .forms import UserCreationForm, CustomUserCreationForm
 from django.contrib.auth import authenticate, login
+from .models import comentario
 
 # Create your views here.
 
@@ -33,3 +34,22 @@ def registro(request):
         
     
     return render(request, 'registration/registro.html', data)
+
+
+def producto(request):
+    """
+    Esta vista carga la lista de comentarios, el 
+    POST request es para cuando se escribe un comentario y
+    el GET request para listarlos en el mismo html
+    """
+    if request.method == 'GET':
+        comentarios = comentario.objects.all()
+        return render(request, 'products/producto.html', {'comentarios': comentarios})
+
+    if request.method == 'POST':
+        texto = request.POST['texto']
+        comments = comentario(usuario=request.user, texto=texto)
+        comments.save()
+        return redirect('producto')
+
+    return render(request, 'products/producto.html')
