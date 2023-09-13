d1ccd9f665d9612b6f58727b5c2242c2d2a5fd8d
sebfigueroap <82173094+sebfigueroap@users.noreply.github.com>
2023-09-13 18:44:35 -0300

    avance xde

--- Prizus/core/urls.py
+++ Prizus/core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import index, login, registro, menu, prueba
+from .views import index, login, registro, menu
 
 urlpatterns = [
     path('', index, name="index"),
     path('registro/', registro, name="registro"),
     path('menu/', menu, name="menu"),
-    path('prueba/', prueba, name="prueba"),
 ]
