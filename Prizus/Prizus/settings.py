7313aa3e72f452e0b6e5082e49cdc157d4b2b95b
Vina del Mar <vina@duoc.acad>
2023-09-14 09:46:03 -0300

    revision del dia 14-09

--- Prizus/Prizus/settings.py
+++ Prizus/Prizus/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = BASE_DIR, 'core/static'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 25
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'resetprizus@gmail.com'
 EMAIL_HOST_PASSWORD = 'nkqzgcoubezwtpqd'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
