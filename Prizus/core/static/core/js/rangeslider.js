ca6d39887a6c43f2ff10b0beeae345797fb6525e
sebfigueroap <82173094+sebfigueroap@users.noreply.github.com>
2023-09-20 22:45:02 -0300

    avance

--- Prizus/core/static/core/js/rangeslider.js
+++ Prizus/core/static/core/js/rangeslider.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
         type: "double", // Dos sliders
         grid: true,
         min: 0, // Valor mínimo
-        max: 10000000, // Valor máximo
+        max: 500000, // Valor máximo
         from: 0, // Valor inicial para el primer slider
-        to: 10000000, // Valor inicial para el segundo slider
+        to: 500000, // Valor inicial para el segundo slider
         skin: "square",
         prefix: "$",
     });
